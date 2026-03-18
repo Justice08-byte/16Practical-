a298df875d070707439b95716922abc28ef3c3ef
4354153 <4354153@myuwc.ac.za>
2026-03-18 14:45:33 +0200

    I have added the catch statements incase there is an error with the file or the file can't be read

--- Heapsort.java
+++ Heapsort.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import java.util.*;
 
-public class Heapsort{
+public class Heapsort {
     // Word Cleaner - reads and cleans words from a file
-    class Wordcleaner{
-        public static ArrayList<String> cleanFile(String filename){
+    class Wordcleaner {
+        public static ArrayList<String> cleanFile(String filename) {
             ArrayList<String> words = new ArrayList<>();
             int lineCount = 0;
-            try (BufferedReader reader = new BufferedReader(new FileReader(filename))){
+            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
                 String line;
-                while ((line = reader.readLine()) != null){
+                while ((line = reader.readLine()) != null) {
                     lineCount++;
                     // Convert to lowercase and split into words
                     String[] rawWords = line.toLowerCase().split("\\s+");
@@ -28,8 +28,22 @@ public class Heapsort{
                         System.out.println("  Processed " + lineCount + " lines, found " + words.size() + " words so far...");
                     }
                 }
+                System.out.println("Completed Load!" + words.size() + "words from" + "joyce1922_ulysses-1.text");
+                System.out.println(" Processed \" + lineCount + \" lines total.");
+            } catch(FileNotFoundException e){
+                System.err.println("Error: File not found - " + "joyce1922_ulysses-1.text");
+                System.err.println("Current directory: " + System.getProperty("user.dir"));
+                System.err.println("Please make sure '" + "joyce1922_ulysses-1.text" + "' is in the current directory.");
+
+            } catch(IOException e){
+                System.err.println("Error reading file: " + "joyce1922_ulysses-1.text");
+                e.printStackTrace();
             }
+            return words;
 
         }
+
+
     }
+
 }
